4a40bdfff0ba6d7f503a95f652ff484a3a06ec9a
krolhm <raphaelb1@icloud.com>
2024-03-23 22:54:33 +0100

    cross compiler shaders

--- shaders/compile.sh
+++ shaders/compile.sh
@@ -1,7 +1,13 @@
 if command -v glslc &> /dev/null; then
     glslc_path=$(command -v glslc)
 else
-    glslc_path=$(find /home/$USER/ -type f -name glslc -executable | head -n 1)
+    if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+        # Linux
+        glslc_path=$(find /home/$USER/ -type f -name glslc -executable | head -n 1)
+    elif [[ "$OSTYPE" == "darwin"* ]]; then
+        # Mac OSX
+        glslc_path=$(find /Users/$USER/ -type f -name glslc -executable | head -n 1)
+    fi
 fi
 
 if [ -n "$glslc_path" ]; then
